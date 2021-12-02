3102e905cff5bb90861151139b11035c809e8c22
davidkharbin <david_k_harbin@hotmail.com>
2021-12-02 14:21:47 -0700

    first commit

---
+++ client/src/components/App.jsx
@@ -0,0 +1,13 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios'
+import '../styles/App.css'
+
+const App = () => {
+	return (
+		<div className="App">
+			<h1>Hello World (source: App.jsx)</h1>
+		</div>
+	)
+}
+
+export default App
---
+++ server/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const path = require('path');
+const port = 3001;
+
+// serve static files from dist dir
+app.use(express.static(path.join(__dirname, '..', 'client', 'dist')));
+app.use(express.json());
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(port, () => {
+  console.log(`Express server listening on port: ${port}`);
+});
+
+app.get('/newRouteHere', (req, res) => {
+  res.send()
+})
+
+app.get('/', (req, res) => {
+  res.sendFile('index.html', { root: __dirname + '/../client/dist' }, (err) => {
+    if (err) {
+      res.status(400).send(err);
+    }
+  });
+});
