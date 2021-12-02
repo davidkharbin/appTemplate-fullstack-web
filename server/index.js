16293b92c2b090c89794cda3018f1c703bfacf64
davidkharbin <david_k_harbin@hotmail.com>
2021-12-02 14:25:20 -0700

    chore: changed server port to 3000

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const path = require('path');
-const port = 3001;
+const port = 3000;
 
 // serve static files from dist dir
 app.use(express.static(path.join(__dirname, '..', 'client', 'dist')));
